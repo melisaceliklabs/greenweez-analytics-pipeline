e85751bd68bc90c4721269bca818f370bc159b95
Melisa Çelik <melisacelikbusiness@proton.me>
2026-08-31 21:58:34 +0000

    dbt-day-3: finance_days mart modeli ve genisletilmis operasyonel model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,13 @@
 select
     o.orders_id,
     o.date_date,
+    o.revenue,
+    o.quantity,
+    o.purchase_cost,
+    o.margin,
+    s.shipping_fee,
+    s.log_cost,
+    s.ship_cost,
     round(
         o.margin + s.shipping_fee - s.log_cost - s.ship_cost
     , 2) as operational_margin
